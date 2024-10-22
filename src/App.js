73b7ae9a0156ed1afc094dfc5b39901cec54d85f
Dante O. Cuales <danteocualesjr@gmail.com>
2024-10-22 18:06:17 +0800

    Refactored code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to My React App</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        // Add a button with an onClick event
+        <button onClick={() => alert('Button clicked!')}>Click me</button>
+
+        // Add a simple counter
+        <Counter />
+
+        // Add a navigation menu
+        <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +35,20 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+  );
+}
+
+// Add this outside the App function
+
+function Counter() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
     </div>
   );
 }
