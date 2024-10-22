efc45cf305f84185deb64837bfe86202e644835e
Dante O. Cuales <danteocualesjr@gmail.com>
2024-10-23 02:51:04 +0800

    Refactored code

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const App = () => {
 
   return (
     <div className="min-h-screen bg-gray-50">
+      
       {/* Navigation Bar */}
       <nav className="bg-white shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
