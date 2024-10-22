2ee79a0735a2f0bad40146b9891fa6d8bc05cd10
Dante O. Cuales <danteocualesjr@gmail.com>
2024-10-22 19:16:23 +0800

    Added TaskCard.js

---
+++ src/Components/TaskCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TaskCard from './components/TaskCard';
+
+function App() {
+  return (
+    <div className="p-8 bg-gray-100 min-h-screen">
+      <TaskCard />
+    </div>
+  );
+}
+
+export default App;
