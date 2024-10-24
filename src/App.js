2b1c2ee0a0f7347eb33558fba8f8d21357fb64a7
Dante O. Cuales <danteocualesjr@gmail.com>
2024-10-24 12:22:36 +0800

    Troubleshooting

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="bg-blue-500 p-8">
-      <h1 className="text-white text-2xl">Test Tailwind</h1>
+    <div style={{padding: '20px'}}>
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello Tailwind
+      </h1>
+      <div className="bg-blue-500 p-4 mt-4">
+        This should be blue
+      </div>
     </div>
   );
 }
